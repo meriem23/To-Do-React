06e6d4e265d33638be5b618db331968c12f8dcbd
meriem23 <esra.msallem@gmail.com>
2020-03-05 16:05:26 +0100

    Updates

---
+++ precache-manifest.07d985f0cc1bde77ec25e55668cf1a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd52231aac9f962fbd2b59f5c6b07639",
+    "url": "/To-Do-React/index.html"
+  },
+  {
+    "revision": "f22d8944b4cc528e43d8",
+    "url": "/To-Do-React/static/css/main.e097edf2.chunk.css"
+  },
+  {
+    "revision": "0cc27525be6fbd42adf1",
+    "url": "/To-Do-React/static/js/2.ab590ffe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/To-Do-React/static/js/2.ab590ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22d8944b4cc528e43d8",
+    "url": "/To-Do-React/static/js/main.8975fe22.chunk.js"
+  },
+  {
+    "revision": "4633771ca8332ae1a1b0",
+    "url": "/To-Do-React/static/js/runtime-main.16424e9e.js"
+  }
+]);
